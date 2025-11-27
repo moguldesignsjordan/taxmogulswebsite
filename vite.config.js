3baece176322786e1d4e0b6f19eaafeff38f25fd
Jordan Cobb <jordancobb92@gmail.com>
2025-11-27 06:18:50 -0400

    Fix Vite SPA routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // This fixes refresh 404 for React Router
+  server: {
+    historyApiFallback: true,
+  },
+
+  // Also required for Vercel production
+  build: {
+    outDir: 'dist',
+  }
+});
